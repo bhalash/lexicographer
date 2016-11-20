8bee7ebced8fa4f5723bfc5a183e0e742f36efe9
Mark <mark@bhalash.com>
2016-11-20 15:17:48 +0000

    Added test suite and populated package.json.

--- test/test.js
+++ test/test.js
@@ -1,15 +1,20 @@
-#!/usr/bin/env node
-
+const tape = require('tape');
 const lexicographer = require('../');
-const array = [...Array(10).keys()];
-const breakpoint = 1000000;
-let permutations = 0;
+const permuter = lexicographer.permute([0, 1, 2]);
 
-for (let value of lexicographer.permute(array)) {
-    permutations++;
+/**
+ * permute() Method
+ * =============================================================================
+ */
 
-    if (permutations === breakpoint) {
-        console.log(value.join(''));
-        break;
-    }
-}
+tape('permute()', assert => {
+    assert.plan(8);
+    assert.equal( typeof lexicographer.permute, 'function');
+    assert.deepEqual(permuter.next().value, [0, 1, 2]);
+    assert.deepEqual(permuter.next().value, [0, 2, 1]);
+    assert.deepEqual(permuter.next().value, [1, 0, 2]);
+    assert.deepEqual(permuter.next().value, [1, 2, 0]);
+    assert.deepEqual(permuter.next().value, [2, 0, 1]);
+    assert.deepEqual(permuter.next().value, [2, 1, 0]);
+    assert.equal(permuter.next().value, undefined);
+});
